37b7e4ef514fbc041a7b741527074ea05c8d5bca
sssgordon <gso7@uwo.ca>
2019-12-02 11:17:09 +0100

    CRUD endpoints for weather

--- weather/router.js
+++ weather/router.js
@@ -3,7 +3,7 @@ const Weather = require("./model");
 
 const router = new Router();
 
-// test endpoints
+// CRUD endpoints
 router.post("/weather", async (req, res, next) => {
   try {
     const weather = {
@@ -11,7 +11,7 @@ router.post("/weather", async (req, res, next) => {
     };
 
     const newWeather = await Weather.create(weather);
-    res.send(newWeather);
+    res.status(201).send(newWeather);
   } catch (error) {
     next(error);
   }
@@ -19,8 +19,36 @@ router.post("/weather", async (req, res, next) => {
 
 router.get("/weather", async (req, res, next) => {
   try {
-    const weather = await Weather.findAll();
+    const weather = await Weather.findAll({ order: [["id", "ASC"]] });
     res.send(weather);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/weather/:id", async (req, res, next) => {
+  try {
+    const weather = await Weather.findByPk(req.params.id);
+    if (weather) {
+      const updatedWeather = await weather.update(req.body);
+      res.status(201).send(updatedWeather);
+    } else {
+      res.status(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/weather/:id", async (req, res, next) => {
+  try {
+    const weather = await Weather.findByPk(req.params.id);
+    if (weather) {
+      const deletedWeather = await weather.destroy();
+      res.status(204).send(deletedWeather);
+    } else {
+      res.status(404);
+    }
   } catch (error) {
     next(error);
   }
